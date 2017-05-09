f183a15eb74bcc229d3e54688eb505926f181634
Psycho Mods <nishsin26@gmail.com>
2017-05-09 13:38:54 +0530

    Updated scripts to build and upload Flashable Zip's to Correct Directories

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,9 +1,16 @@
+#!/bin/sh
+
+VER="-$(date +"%Y-%m-%d"-%H%M)-"
+CODE="Experi"
+BASEVER="Psycho-"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
   mkdir $HOME/Builds
-  cp -R $HOME/kernel/arch/arm64/boot/Image.gz-dtb $HOME/Builds
+  cd $HOME/kernel/flashable
+  zip -r `echo $BASEVER$CODE$VER`.zip *
+  mv `echo $BASEVER$CODE$VER`.zip $HOME/Builds
 
   #go to home and setup git
   cd $HOME
@@ -14,13 +21,12 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/psycho-source/Psycho-Kernel.git  gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
-  cd gh-pages
-  cp -Rf $HOME/Builds/* .
+  cp -Rf $HOME/Builds/* $HOME/gh-pages/Experimental/
 
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Done magic with coverage\n"
+  echo -e "Done\n"
 fi
